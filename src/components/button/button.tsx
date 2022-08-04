d5922025a07e7b4347ebcf7dae2ea3d6d5206013
Dmitry <62187726+Solowayer@users.noreply.github.com>
2022-08-04 20:12:42 +0300

    Adding storybook

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,12 @@
+import { ReactNode } from 'react'
 import { StyledButton } from './styled'
 
-export const Button = () => {
-  return <StyledButton>Cringe</StyledButton>
+interface ButtonProps {
+  type?: 'primary' | 'secondary' | 'tretiary' | 'disabled'
+  children: ReactNode
+}
+
+export const Button: React.FC<ButtonProps> = (props) => {
+  const { children } = props
+  return <StyledButton>{children}</StyledButton>
 }
